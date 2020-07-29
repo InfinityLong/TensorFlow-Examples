6ddb9871d884a64402d1c57f4354fd1161254079
InfinityLong <2840478265@qq.com>
2020-07-29 20:34:28 +0800

    add Hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File   : hello.py
+@Desc   : This is file Description
+@Time   : 2020/07/29 20:32:09
+@Author : Zero2Inf
+@Depart : Shandong University
+@Contact: 2840478275@qq.com
+@Version: 3.7
+'''
+
+# here put the import lib
+
+
+print("Hello World")
